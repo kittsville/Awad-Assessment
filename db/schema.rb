ccfbbb5ac2b197259dc2990292a9cd74e82800e8
Kit <kittsville@googlemail.com>
2015-03-28 17:46:01 +0000

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328165034) do
+ActiveRecord::Schema.define(version: 20150328173918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,8 @@ ActiveRecord::Schema.define(version: 20150328165034) do
   create_table "subscriptions", force: :cascade do |t|
     t.integer  "user_id"
     t.text     "feed"
-    t.datetime "subscriptiondate"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
