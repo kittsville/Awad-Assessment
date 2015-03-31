4c2becb608323827a066d8a09f31e2dc1665b677
Kit <kittsville@googlemail.com>
2015-03-31 16:03:22 +0100

    Added User model constraints
    Added pre-validating down-casing of User model username and email to make future queries easier

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
+  # Forces username/email to be lower-case before addition to the database
+  def downcase_details
+    self.username	= self.username.downcase	if self.username.present?
+    self.email		= self.email.downcase		if self.email.present?
+  end
+  
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 40 }
   EMAIL_VALIDATOR = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # A simple email address validing Regex from Ruby on Rails Tutorial (3rd Ed.) by Michael Hartl
   validates :email, presence: true, length: { maximum: 255 }, format: { with: EMAIL_VALIDATOR }
+  
+  private :downcase_details
 end
