f3358db74479a1c73edd9d375c7e94e5e7de0dd6
Kit <kittsville@googlemail.com>
2015-03-31 13:35:10 +0100

    Implemented username/email restrictions
    Added restrictions to the nature of username/email parameters to pass some of the user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
-end
+  validates :username, presence: true, uniqueness: true, length: { maximum: 40 }
+  EMAIL_VALIDATOR = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # A simple email address validing Regex from Ruby on Rails Tutorial (3rd Ed.) by Michael Hartl
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: EMAIL_VALIDATOR }
