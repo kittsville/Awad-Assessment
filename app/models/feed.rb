ccd63e51070778eba79747175b6615c9c442f39f
Kit <kittsville@googlemail.com>
2015-03-31 20:43:57 +0100

    Passed Feed model tests
    Added Feed model content restrictions as to pass the tests

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,13 @@
 class Feed < ActiveRecord::Base
+  def downcase_url
+    self.url = self.url.downcase if self.url.present?
+  end
+  
+  before_validation :downcase_url
+  
+  validates :title, presence: true, length: { maximum: 150 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :url, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
+
+  private :downcase_url
 end
