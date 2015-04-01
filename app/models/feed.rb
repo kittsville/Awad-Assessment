d06bb0e1212185a41e127ce5fae8690a23446ad1
Kit <kittsville@googlemail.com>
2015-04-01 15:46:31 +0100

    Added Feed constraints
    Added contraint to Feeds model so that the blacklisted attribute must be set and defaults to false if unset

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,11 +3,16 @@ class Feed < ActiveRecord::Base
     self.url = self.url.downcase if self.url.present?
   end
   
-  before_validation :downcase_url
+  def default_values
+    self.blacklisted = false if self.blacklisted.nil?
+  end
+  
+  before_validation :downcase_url, :default_values
   
   validates :title, presence: true, length: { maximum: 150 }
   validates :description, presence: true, length: { maximum: 1000 }
   validates :url, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
+  validates :blacklisted, presence: true
 
   private :downcase_url
 end
