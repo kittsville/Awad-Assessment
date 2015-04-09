fe026dd76641f71330ea562a10cc112a3c510b6d
Kit <kittsville@users.noreply.github.com>
2015-04-09 17:22:34 +0100

    Added Mandril Connection Details
    Added configuration details for production database to send mail using Mandril

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+   # Send mail using mandrill
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.mandrillapp.com",
+    :port => 25, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => ENV['MANDRILL_USER'],
+    :password => ENV['MANDRILL_PASSWORD'], # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'awad.sci1.uk', # your domain to identify your server when connecting
+}
 end
