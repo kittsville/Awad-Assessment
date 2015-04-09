fd3b9afbebd4f0c0a546a1d16b4e6a9c0f663a8b
Kit <kittsville@googlemail.com>
2015-04-09 21:40:22 +0000

    Propagated Description Removal
    Removed descriptions from seed Feeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 [
-  {title: 'BBC News Scotland', description: 'The latest stories from the Scotland section of the BBC News web site', url: 'http://feeds.bbci.co.uk/news/scotland/rss.xml'},
-  {title: 'BBC Top UK Stories', description: 'The top stories from the UK', url: 'http://feeds.bbci.co.uk/news/rss.xml?edition=uk'},
-  {title: 'The Telegraph Sport', description: 'Latest sport news, fixtures and match reports from columnists Henry Winter, Alan Hansen, Paul Hayward and more.', url: 'http://www.telegraph.co.uk/sport/rss'},
-  {title: 'Simple Recipes', description: 'A family cooking and food blog with hundreds of healthy, whole-food recipes for the home cook. Photographs, easy-to-follow instructions, and reader comments.', url: 'http://feeds.feedburner.com/elise/simplyrecipes'},
-  {title: 'Tales from Tech Support', description: 'A Subreddit dedicated to stories of tech woe, difficult clients, moral support and pure luck', url: 'http://www.reddit.com/r/talesfromtechsupport/hot.rss'},
-  {title: 'University of Aberdeen Library News', description: "The latest news from the University of Aberdeen's Library, Special Collections and Museums", url: 'http://www.abdn.ac.uk/library/news-events/news/rss.xml'}
+  {title: 'BBC News Scotland', url: 'http://feeds.bbci.co.uk/news/scotland/rss.xml'},
+  {title: 'BBC Top UK Stories', url: 'http://feeds.bbci.co.uk/news/rss.xml?edition=uk'},
+  {title: 'The Telegraph Sport', url: 'http://www.telegraph.co.uk/sport/rss'},
+  {title: 'Simple Recipes', url: 'http://feeds.feedburner.com/elise/simplyrecipes'},
+  {title: 'Tales from Tech Support', url: 'http://www.reddit.com/r/talesfromtechsupport/hot.rss'},
+  {title: 'University of Aberdeen Library News', url: 'http://www.abdn.ac.uk/library/news-events/news/rss.xml'}
 ].each do |feed|
   feedAR = Feed.new(feed)
   
