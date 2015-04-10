c38a856992f24e89dcd21c8864433b19fae0f936
Kit <kittsville@googlemail.com>
2015-04-10 17:12:20 +0000

    Added more RSS Feeds,
    Added Aberdeen City Council RSS feed to seed feeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
   {title: 'BBC News Scotland', url: 'http://feeds.bbci.co.uk/news/scotland/rss.xml'},
   {title: 'BBC Top UK Stories', url: 'http://feeds.bbci.co.uk/news/rss.xml?edition=uk'},
   {title: 'The Telegraph Sport', url: 'http://www.telegraph.co.uk/sport/rss'},
+  {title: 'Aberdeen City Council News', url: 'http://www.aberdeencity.gov.uk/accnews.xml'},
   {title: 'Simple Recipes', url: 'http://feeds.feedburner.com/elise/simplyrecipes'},
   {title: 'Tales from Tech Support', url: 'http://www.reddit.com/r/talesfromtechsupport/hot.rss'},
   {title: 'University of Aberdeen Library News', url: 'http://www.abdn.ac.uk/library/news-events/news/rss.xml'}
