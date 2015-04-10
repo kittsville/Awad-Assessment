fa37ea42c99a8d98b2e66290113cd8bce5c1618f
Kit <kittsville@googlemail.com>
2015-04-10 12:53:13 +0000

    Added feed subscription status
    Added serverside processing of feed subscription status so client-side knows if a feed is subscribed to, not subscribed to or the user is not signed in
    Added user signed in status to feed fetching
    Added subscribe/unsubscribe icons to feed boxes which display depending on feed status relative to current signed in or lack of signed in user
    Styled new sub/unsub icons

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,18 +1,34 @@
 function feed(feedObj, parentElement) {
-	rawFeed	= feedObj;
-	achor	= parentElement;
-	gFeed	= new google.feeds.Feed(rawFeed.url);
-	
-	feedElement = $('<div/>',{
+	var rawFeed    = feedObj[1];
+  var subscribed = feedObj[0];
+	var anchor     = parentElement;
+	var gFeed      = new google.feeds.Feed(rawFeed.url);
+  
+	var feedElement = $('<div/>',{
 		'class'	: 'panel panel-default single-feed',
 		'id'	: 'feed-'+rawFeed.id,
 		'html'	: [
 			$('<div/>',{
 				'class'	: 'panel-heading',
-				'html'	: $('<h3/>',{
-					'class'	: 'panel-title',
-					'html'	: rawFeed.title
-				})
+				'html'	: [
+          $('<h3/>',{
+					  'class'	: 'panel-title',
+					  'html'	: rawFeed.title
+				  }),
+          $('<div/>',{
+            'class'  : 'sub-icons',
+            'html'   : [
+              $('<span/>',{
+                'class'      : 'glyphicon glyphicon-heart subscribe-icon',
+                'aria-hidden': 'true'
+              }),
+              $('<span/>',{
+                'class'      : 'glyphicon glyphicon-remove unsubscribe-icon',
+                'aria-hidden': 'true'
+              })
+            ] 
+          })
+        ]
 			}),
 			$('<div/>',{
 				'class'	: 'panel-body',
@@ -26,24 +42,41 @@ function feed(feedObj, parentElement) {
 				'html'	: rawFeed.url
 			})
 		]
-	}).appendTo(achor);
+	}).appendTo(anchor);
+  
+  var subscribeIcon    = feedElement.find('span.subscribe-icon');
+  var unsubscribeIcon  =  feedElement.find('span.unsubscribe-icon');
 	
 	var body = feedElement.find('div.panel-body');
+  
+  // Switches which icon is displayed depending on whether the user is subscribed to a feed
+  function changeDisplayedIcon(){
+    if ( subscribed === true ) {
+      subscribeIcon.hide();
+      unsubscribeIcon.show();
+    } else if ( subscribed === false ) {
+      subscribeIcon.show();
+      unsubscribeIcon.hide();
+    } else {
+      subscribeIcon.hide();
+      unsubscribeIcon.hide();
+    }
+  }
 	
 	function renderPosts(data){
-		posts = [];
+		var posts = [];
 		
 		data.feed.entries.forEach(function(post,p){
-			postTitle = $('<h4/>',{html:post.title});
+			var postTitle = $('<h4/>',{html:post.title});
 			
 			if (post.hasOwnProperty('link') && post.link !== '') {
-				postTitle = $('<a/>',{
+				var postTitle = $('<a/>',{
 					href	: post.link,
 					html	: postTitle
 				});
 			}
 			
-			postAuthor = []
+			var postAuthor = []
 			
 			if (post.hasOwnProperty('author') && post.author !== '') {
 				postAuthor.push($('<p/>',{
@@ -68,11 +101,12 @@ function feed(feedObj, parentElement) {
 		body.html(posts);
 	}
 	
+  changeDisplayedIcon();
 	gFeed.load(renderPosts);
 }
 
 function feedEngine(requestType) {
-	feeds		= []
+	var feeds		= []
 		
 	$.ajax({
 		type	: 'GET',
@@ -82,7 +116,9 @@ function feedEngine(requestType) {
 	}).success(function(data){
 		parentElement	= $('div.feed-bin');
 		
-		data.forEach(function(feedObj,i){
+    user_signed_in = data[0];
+    
+		data[1].forEach(function(feedObj,i){
 			feeds.push(new feed(feedObj, parentElement));
 		});
 	}).fail(function(){
@@ -90,8 +126,7 @@ function feedEngine(requestType) {
 	});
 }
 
-function strip(html)
-{
+function strip(html) {
    var tmp = document.createElement("DIV");
    tmp.innerHTML = html;
    return tmp.textContent || tmp.innerText || "";
