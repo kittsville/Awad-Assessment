6332ebecec16ce4ba89643f23e27f7c0f9da0997
Kit <kittsville@googlemail.com>
2015-04-10 16:41:26 +0000

    Added default users to seed
    Added a test user and a test admin user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# Seeds Feeds (heh)
 [
   {title: 'BBC News Scotland', url: 'http://feeds.bbci.co.uk/news/scotland/rss.xml'},
   {title: 'BBC Top UK Stories', url: 'http://feeds.bbci.co.uk/news/rss.xml?edition=uk'},
@@ -11,4 +12,20 @@
   puts "Adding Feed: #{feedAR.title}"
   
   feedAR.save
+end
+
+# Seeds Users
+[
+  {username: 'kittsville', email: 'kittsville@googlemail.com', password: 'swordfish', password_confirmation: 'swordfish'},
+  {username: 'neo', email: 'neo@nebuchadnezzar.com', password: 'keymaster', password_confirmation: 'keymaster', admin: 1}
+].each do |user|
+  userObj = User.new(user)
+  
+  if userObj.valid?
+    puts "Creating default user #{userObj.username}"
+    userObj.save
+  else
+    puts "Unable to create default user #{userObj.username} because:"
+    puts userObj.errors.full_messages
+  end
 end
